d17fdd5c9390182c8b380ca398cc863494636fc6
chihaia dragos <dragos.chihaia.gabriel@gmail.com>
2024-03-13 19:44:21 +0200

    first commit

---
+++ src/main/java/ro/sda/java64/firstDemo/PrimulProiectSpringBootApplication.java
@@ -0,0 +1,13 @@
+package ro.sda.java64.firstDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimulProiectSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimulProiectSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/sda/java64/firstDemo/PrimulProiectSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.sda.java64.firstDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimulProiectSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
